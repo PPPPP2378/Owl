636ecb682d4842812c8f516c6548b91713f16288
DESKTOP-QA5JM5E\GA2A <h1202515017@hiratagakuen.onmicrosoft.com>
2026-06-01 12:42:54 +0900

    kome

--- Owl/Assets/nisimoto/script/Player/Owlaction_n.cs
+++ Owl/Assets/nisimoto/script/Player/Owlaction_n.cs
@@ -1,6 +1,52 @@
 using UnityEngine;
 
-public class Owlaction_n
+public class Owlaction_n : MonoBehaviour
 {
-    
+    public float detectRange = 3f;
+
+    public AudioSource audioSource;
+    public AudioClip owlCry;
+
+    private bool mysteryDetected = false;
+
+    void Update()
+    {
+        Collider2D[] hits = Physics2D.OverlapCircleAll(
+            transform.position,
+            detectRange
+        );
+
+        bool foundMystery = false;
+
+        foreach (Collider2D hit in hits)
+        {
+            if (hit.CompareTag("Mystery"))
+            {
+                foundMystery = true;
+
+                if (!mysteryDetected)
+                {
+                    audioSource.PlayOneShot(owlCry);
+                    mysteryDetected = true;
+                }
+
+                break;
+            }
+        }
+
+        if (!foundMystery)
+        {
+            mysteryDetected = false;
+        }
+    }
+
+    // SceneÉrÉÖÅ[Ç≈îÕàÕï\é¶
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(
+            transform.position,
+            detectRange
+        );
+    }
 }
