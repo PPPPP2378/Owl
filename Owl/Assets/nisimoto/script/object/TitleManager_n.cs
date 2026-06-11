d2a52b751128bc1f5e0213e32a086b782597f5f5
DESKTOP-QA5JM5E\GA2A <h1202515017@hiratagakuen.onmicrosoft.com>
2026-06-11 16:32:21 +0900

    未完

--- Owl/Assets/nisimoto/script/object/TitleManager_n.cs
+++ Owl/Assets/nisimoto/script/object/TitleManager_n.cs
@@ -1,6 +1,45 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
 
-public class TitleManager_n
+public class TitleManager_n : MonoBehaviour
 {
-    
+    private int selectID = 0;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            selectID--;
+
+            if (selectID < 0)
+                selectID = 2;
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            selectID++;
+
+            if (selectID > 2)
+                selectID = 0;
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            switch (selectID)
+            {
+                case 0:
+                    Debug.Log("START");
+                    break;
+
+                case 1:
+                    Debug.Log("MENU");
+                    break;
+
+                case 2:
+                    Debug.Log("EXIT");
+                    break;
+            }
+        }
+    }
 }
