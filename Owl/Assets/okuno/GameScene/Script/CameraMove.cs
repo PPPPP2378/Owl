9c22873be37a78a30eed020ffd077b328e364560
DESKTOP-0TPKAPQ\GA2A <h1202519012@hiratagakuen.onmicrosoft.com>
2026-07-31 14:44:51 +0900

    Ending追加

---
+++ Owl/Assets/okuno/GameScene/Script/CameraMove.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using TMPro;
+
+public class CameraMove : MonoBehaviour
+{
+    public float speed = 2f;
+    public float stopY = 10f;
+
+    public GameObject finText;   // Finのオブジェクト
+
+    private AudioSource audioSource;
+    private bool stopped = false;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+
+        if (finText != null)
+            finText.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (!stopped)
+        {
+            Vector3 pos = transform.position;
+            pos.y += speed * Time.deltaTime;
+
+            if (pos.y >= stopY)
+            {
+                pos.y = stopY;
+                stopped = true;
+
+                audioSource.Play();
+
+                if (finText != null)
+                    finText.SetActive(true);
+            }
+
+            transform.position = pos;
+        }
+    }
+}
